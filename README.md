7b50b0514fa547e116c54a80087c4e249e693d7a
S-S-Bros101 <177675144+S-S-Bros101@users.noreply.github.com>
2025-12-11 19:36:13 -0500

    Final commit
    Added more info, and fixed breathing brackets

--- README.md
+++ README.md
@@ -4,10 +4,13 @@
 
 <!-- This site is created with html, css, and js... yea thats about it -->
 
+<!-- Committed 12/11/2025 1:30pm est -->
+
 <div align="center">
   <a href="https://moonshot.hackclub.com" target="_blank">
     <img src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/35ad2be8c916670f3e1ac63c1df04d76a4b337d1_moonshot.png" 
          alt="This project is part of Moonshot, a 4-day hackathon in Florida visiting Kennedy Space Center and Universal Studios!" 
          style="width: 100%;">
+         <!-- This is the moonshot logo to get them hours yall! -->
   </a>
 </div>
--- main.js
+++ main.js
@@ -1,41 +1,85 @@
-let element = ["h1", "h2", "h3", "h4", "h5", "h6"];
-let ElementContent = ["Who is Raiden?", "Me likes", "Languages I code in", "Where am I?", 
-"Projects", "My cat :D"
-];
+// let element = ["h1", "h2", "h3", "h4", "h5", "h6", "p1", "p2", "p3", "p4"];
+// let ElementContent = [
+//     "Who is Raiden?",
+//     "Me likes",
+//     "Languages I code in",
+//     "Where am I?",
+//     "Projects",
+//     "My cat :D",
+//     "I like hardware and soldering. I really like eating food...",
+//     "I code in js, C#, C++, and Java.",
+//     "I live in Ontario, Canada :)",
+//     "I dont have many to share really..."
+// ];
 
-let InfoContent = ["I like hardware and soldering.I really like eating food around the "+
-    "world, right now my top favourites are ramen, jollof rice.", "I code in js, C#, C++, "+
-    "and java.", "I live in Ontario, Canada :&#41;", "I dont have many to share really, but"+
-    " currently im working on arduino, web dev, and soldering hardware"
-];
+// let InfoContent = ["I like hardware and soldering.I really like eating food around the "+
+//     "world, right now my top favourites are ramen, jollof rice.", "I code in js, C#, C++, "+
+//     "and java.", "I live in Ontario, Canada :&#41;", "I dont have many to share really, but"+
+//     " currently im working on arduino, web dev, and soldering hardware"
+// ];
 
+//I had to gpt this bit bc it got too confusing :sadge:
+let elements = document.querySelectorAll('.breathing');
 let blink = false;
 
-//this loops through all of them instead of adding them manually T-T
-for(let i=0; i < ElementContent.length; i++){
-    let part = document.getElementById(element[i]);
-    part.addEventListener("mouseover", ()=> breathe(part,false,i));
-    part.addEventListener("mouseleave", () => breathe(part,true,i));
-}
+elements.forEach((el) => {
+    
+    el.dataset.original = el.textContent;//grab the orginal text, more efficient
+});
 
-//This one sets teh breathing brackets according to the var block (dont show) and
-//blink (show it closed and oped :nodnod:)
-function breathe(cont, block,num){
-    let htmlContent = ElementContent[num];
-    //first terminal case here!
+
+elements.forEach((el) => {
+    
+    el.addEventListener("mouseover", () => breathe(el, false));//breathing 
+    el.addEventListener("mouseleave", () => breathe(el, true));//not breathing
+});
+
+
+function breathe(elementThing, /*OH NO HIS IS BAD*/ block){
+
+    //if not hovered over, stop breathing
+
     if(block){
-        cont.innerHTML = htmlContent;
+        elementThing.textContent = elementThing.dataset.original;
         return;
     }
-    //if blink, then shrink
+    
     if(blink){
-        cont.innerHTML = "> "+htmlContent+"<";
+        elementThing.textContent = `>${elementThing.dataset.original} <`;
     }else{
-        //Show breathing brackets closed
-        cont.innerHTML = ">"+htmlContent+"<";
+        elementThing.textContent = `>${elementThing.dataset.original}<`;
     }
 }
 
+// let blink = false;
+// //Init blink
+
+// //this loops through all of them instead of adding them manually T-T
+// for(let i=0; i < ElementContent.length; i++){
+//     let part = document.getElementById(element[i]);
+//     part.addEventListener("mouseover", ()=> breathe(part,false,i));
+//     part.addEventListener("mouseleave", () => breathe(part,true,i));
+// }
+
+// //This one sets teh breathing brackets according to the var block (dont show) and
+// //blink (show it closed and oped :nodnod:)
+// function breathe(cont, block,num){
+//     let htmlContent = ElementContent[num];
+//     //first terminal case here!
+//     if(block){
+//         cont.innerHTML = htmlContent;
+//         return;
+//     }
+//     //if blink, then shrink
+//     if(blink){
+//         cont.innerHTML = "> "+htmlContent+" <";
+//     }else{
+    //         //Show breathing brackets closed
+//         cont.innerHTML = ">"+htmlContent+"<";
+//     }
+// }
+
 setInterval(() => {
-    blink = !blink;
+    blink = !blink;//reset blink every interval!!!
+    document.querySelectorAll('.breathing:hover').forEach(el => breathe(el, false));
 }, 500);
